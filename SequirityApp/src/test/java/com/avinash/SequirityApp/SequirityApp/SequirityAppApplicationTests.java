23a5209d9ef80ddc483cb7e3a74bbcfb671c583d
avinash <avinashtiwari2405@gmail.com>
2024-11-28 00:57:42 +0530

    added spring security configuration

---
+++ SequirityApp/src/test/java/com/avinash/SequirityApp/SequirityApp/SequirityAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avinash.SequirityApp.SequirityApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SequirityAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
