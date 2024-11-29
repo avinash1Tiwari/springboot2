eaa8f6094b52a9da3095b3555e499dc28cd35f58
avinash tiwari <avinash.tiwari@rapipay.com>
2024-11-29 13:32:08 +0530

    5.4.0

--- SequirityApp/src/main/java/com/avinash/SequirityApp/SequirityApp/entities/User.java
+++ SequirityApp/src/main/java/com/avinash/SequirityApp/SequirityApp/entities/User.java
@@ -1,15 +1,23 @@
 package com.avinash.SequirityApp.SequirityApp.entities;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.envers.Audited;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 import java.util.List;
-
+@Entity
+@Table(name = "app_user")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Audited
 public class User implements UserDetails {                     // UserDetails comes from springframework.security.core
 //commit from 70
 //    again commit from 70
