bf76ff213fa6853cad540b9f2fdbcf7b96a090e8
avinash tiwari <avinash.tiwari@rapipay.com>
2024-12-09 09:45:00 +0530

    completed 5.8 as completed module-5 and learnt how to handle unauthorised credidential and JWtExceptions

--- SequirityApp/src/main/java/com/avinash/SequirityApp/SequirityApp/filters/JwtAuthFilter.java
+++ SequirityApp/src/main/java/com/avinash/SequirityApp/SequirityApp/filters/JwtAuthFilter.java
@@ -13,6 +13,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 
 import java.io.IOException;
 @Component
@@ -21,35 +22,44 @@ public class JwtAuthFilter extends OncePerRequestFilter {
 
     private final JwtService jwtService;
     private final UserService userService;
+    private final HandlerExceptionResolver handlerExceptionResolver;
+
+//    throws ServletException, IOException
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-
-        final String requestTokenHeader = request.getHeader("Authorization");
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) {
 
 
-        if(requestTokenHeader == null || !requestTokenHeader.startsWith("Bearer")){
-            filterChain.doFilter(request,response);    /// allow to go to next filter
-            return;
+        try {
+            final String requestTokenHeader = request.getHeader("Authorization");
+
+
+            if (requestTokenHeader == null || !requestTokenHeader.startsWith("Bearer")) {
+                filterChain.doFilter(request, response);    /// allow to go to next filter
+                return;
+            }
+
+            String token = requestTokenHeader.split("Bearer ")[1];    //// token comes like "Bearer asfjfklvfl,lf"
+
+            Long userId = jwtService.getUserIdFromToken(token);
+
+            if (userId != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+
+                User user = userService.getUserById(userId);
+                UsernamePasswordAuthenticationToken authenticationToken =
+                        new UsernamePasswordAuthenticationToken(user, null, null);   // here we pass password = null, role = null.
+
+                authenticationToken.setDetails(
+                        new WebAuthenticationDetailsSource().buildDetails(request)
+                );
+
+                SecurityContextHolder.getContext().setAuthentication(authenticationToken);      //// add this user in spring security context-holder.
+            }
+
+            filterChain.doFilter(request, response);
+        } catch(Exception ex)
+        {
+            handlerExceptionResolver.resolveException(request,response,null,ex);
         }
-
-        String token = requestTokenHeader.split("Bearer ")[1];    //// token comes like "Bearer asfjfklvfl,lf"
-
-        Long userId = jwtService.getUserIdFromToken(token);
-
-        if(userId != null && SecurityContextHolder.getContext().getAuthentication() == null){
-
-            User user = userService.getUserById(userId);
-            UsernamePasswordAuthenticationToken authenticationToken =
-                    new UsernamePasswordAuthenticationToken(user,null,null);   // here we pass password = null, role = null.
-
-            authenticationToken.setDetails(
-                    new WebAuthenticationDetailsSource().buildDetails(request)
-            );
-
-            SecurityContextHolder.getContext().setAuthentication(authenticationToken);      //// add this user in spring security context-holder.
-        }
-
-        filterChain.doFilter(request,response);
     }
 }
