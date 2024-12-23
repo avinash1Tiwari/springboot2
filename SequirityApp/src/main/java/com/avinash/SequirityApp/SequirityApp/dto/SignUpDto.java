86d375d894fdb98c0365bdf64b85e1ccdca45bc4
avinash <avinashtiwari2405@gmail.com>
2024-12-23 20:44:45 +0530

    understood role-based authentication

--- SequirityApp/src/main/java/com/avinash/SequirityApp/SequirityApp/dto/SignUpDto.java
+++ SequirityApp/src/main/java/com/avinash/SequirityApp/SequirityApp/dto/SignUpDto.java
@@ -1,7 +1,10 @@
 package com.avinash.SequirityApp.SequirityApp.dto;
 
 
+import com.avinash.SequirityApp.SequirityApp.enums.Role;
 import lombok.Data;
+
+import java.util.Set;
 
 @Data
 
@@ -10,5 +13,6 @@ public class SignUpDto {
     private String email;
     private String password;
     private String name;
+    private Set<Role> roles;
 
 }
